210b39d212bdd48ff00d61292de4b2b36c3335ab
arialghifari <arialghifari99@gmail.com>
2022-04-01 04:23:43 +0800

    Protecting routes & get current login user data

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -73,9 +73,9 @@ const loginUser = asyncHandler(async (req, res) => {
 // @route		GET /api/users/me
 // @access	Private
 const getMe = asyncHandler(async (req, res) => {
-  res.json({
-    message: "Current user data",
-  });
+  const { _id, name, email } = await User.findById(req.user.id);
+
+  res.status(200).json({ id: _id, name, email });
 });
 
 // Generate JWT
