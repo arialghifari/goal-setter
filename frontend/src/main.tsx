a82e398be3d5662c77d5f2d014d937aa03e1df40
arialghifari <arialghifari99@gmail.com>
2022-04-06 23:19:57 +0800

    Redux start & auth slice

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,15 @@
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
+import React from "react";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
