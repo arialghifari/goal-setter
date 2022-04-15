456d07826cf2c835d5816ebe30b313e598a52eaf
arialghifari <arialghifari99@gmail.com>
2022-04-15 21:10:26 +0800

    Add login function

--- frontend/src/features/auth/authSlice.ts
+++ frontend/src/features/auth/authSlice.ts
@@ -12,8 +12,13 @@ const initialState = {
   message: "",
 };
 
-interface User {
+interface RegisterUser {
   name: string;
+  email: string;
+  password: string;
+}
+
+interface LoginUser {
   email: string;
   password: string;
 }
@@ -21,9 +26,28 @@ interface User {
 // Register user
 export const register = createAsyncThunk(
   "auth/register",
-  async (user: User, thunkAPI) => {
+  async (user: RegisterUser, thunkAPI) => {
     try {
       return await authService.register(user);
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+
+// Login user
+export const login = createAsyncThunk(
+  "auth/login",
+  async (user: LoginUser, thunkAPI) => {
+    try {
+      return await authService.login(user);
     } catch (error) {
       const message =
         (error.response &&
@@ -59,6 +83,20 @@ export const authSlice = createSlice({
         state.user = action.payload;
       })
       .addCase(register.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload.toString();
+        state.user = null;
+      })
+      .addCase(login.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(login.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.user = action.payload;
+      })
+      .addCase(login.rejected, (state, action) => {
         state.isLoading = false;
         state.isError = true;
         state.message = action.payload.toString();
